e9c26f1fc956d858dd5d01b3483ffbc408ac7d28
Сергей Шураев <128598320+JordanLaurent09@users.noreply.github.com>
2023-03-24 21:23:07 +0200

    Обновление

--- CifraLearning/ZeroProject/Program.cs
+++ CifraLearning/ZeroProject/Program.cs
@@ -8,10 +8,12 @@ namespace ZeroProject
         {
             string _line;
             StringBuilder _valueStringBuilder = new StringBuilder();
-
+            float _value = 0;
             // ввод
             Console.WriteLine("Enter the line");
             _line = Console.ReadLine();
+
+            ParseLine(true);
 
             // обработка
             for (int i = 0; i < _line.Length; i++)
@@ -20,11 +22,42 @@ namespace ZeroProject
                 if (char.IsDigit(letter))
                 {
                     _valueStringBuilder.Append(letter);
+                } else if (letter == '.' || letter == ',') {
+                    _valueStringBuilder.Append(letter);
                 }
             }
 
-            Console.WriteLine(_valueStringBuilder.ToString());  
+            if (_valueStringBuilder.Length == 0)
+            {
+                Console.WriteLine("The number has not found");
+            }
+            else
+            {
+                _value = float.Parse(_valueStringBuilder.ToString());
+            }
+
             // вывод
+
+            Console.WriteLine($"Number is {_value}");
+
+
+            void ParseLine(bool exitOnFirstLetter)
+            {
+                for (int i = 0; i < _line.Length; i++)
+                {
+                    char letter = _line[i];
+                    if (char.IsDigit(letter))
+                    {
+                        _valueStringBuilder.Append(letter);
+                        continue;
+                    }
+                    else if (letter == '.' || letter == ',')
+                    {
+                        _valueStringBuilder.Append(',');
+                        continue;
+                    }
+                }
+            }
         }
     }
 }
